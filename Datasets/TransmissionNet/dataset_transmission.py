79668aa874ad3fd4aa604b7d0101072840e04149
RaelCappra <rael.cappra@gmail.com>
2017-10-20 10:25:54 -0200

    Documenting dataset_transmission

--- Datasets/TransmissionNet/dataset_transmission.py
+++ Datasets/TransmissionNet/dataset_transmission.py
@@ -1,14 +1,16 @@
 #pylint: disable=E0611,E0401,C0325
+"""Contem a classe DatasetTransmission"""
 import os
 import random
 
 import tensorflow as tf
 
 import Datasets.TransmissionNet.simulator as simulator
-
 import dataset
 
 class DatasetTransmission(dataset.Dataset):
+    """Define a estrutura para extracao de patches
+    para o treinamento e validacao da TransmissionNet"""
     def __init__(self):
         parameters_list = ["train_path", "test_path", "input_size", "output_size", "turbidity_path",
                            "turbidity_size", "patch_size", "trans_minval", "trans_maxval"]
@@ -31,6 +33,15 @@ class DatasetTransmission(dataset.Dataset):
             self.turbidity_path, self.turbidity_size, self.batch_size, 0, 0, self.sess)
 
     def read_and_decode(self, filename_queue):
+        """
+        args:
+            filename_queue:
+                the filename_queue to read from
+
+        returns:
+            a tensor of type float32 with the images read and decoded
+            from the filename queue
+        """
         reader = tf.TFRecordReader()
         _, serialized_example = reader.read(filename_queue)
         features = tf.parse_single_example(
@@ -59,10 +70,10 @@ class DatasetTransmission(dataset.Dataset):
         offset_y = random.randint(0, self.input_size[1] - size_y - 1)
 
         image = image[offset_x:offset_x + size_x, offset_y:offset_y+size_y]
-        transmission = self.random_transmissions(1)
+        transmission = self._random_transmissions(1)
         return image, transmission
 
-    def random_transmissions(self, batch_size):
+    def _random_transmissions(self, batch_size):
         minval = self.config_dict["trans_minval"]
         maxval = self.config_dict["trans_maxval"]
         transmissions = tf.random_uniform([batch_size], minval=minval, maxval=maxval)
@@ -71,10 +82,8 @@ class DatasetTransmission(dataset.Dataset):
     def next_batch_train(self, initial_step):
         """
         args:
-            batch_size:
-                number of examples per returned batch
-            num_epochs:
-                number of time to read the input data
+            initial_step:
+                step to start restoring from
 
         returns:
             a tuple(image, transmissions) where:
@@ -84,14 +93,14 @@ class DatasetTransmission(dataset.Dataset):
 
         filenames = self.train_file
 
-        dataset = tf.contrib.data.TFRecordDataset(filenames)
-        dataset = dataset.map(self._parse_function)  # Parse the record into tensors.
-        dataset = dataset.shuffle(buffer_size=3000)
-        dataset = dataset.batch(self.config_dict["batch_size"])
-        dataset = dataset.repeat(self.config_dict["num_epochs"])  # Repeat the input.
-        dataset = dataset.skip(initial_step)
+        dset = tf.contrib.data.TFRecordDataset(filenames)
+        dset = dset.map(self._parse_function)  # Parse the record into tensors.
+        dset = dset.shuffle(buffer_size=3000)
+        dset = dset.batch(self.config_dict["batch_size"])
+        dset = dset.repeat(self.config_dict["num_epochs"])  # Repeat the input.
+        dset = dset.skip(initial_step)
 
-        iterator = dataset.make_one_shot_iterator()
+        iterator = dset.make_one_shot_iterator()
 
         images, transmissions = iterator.get_next()
 
@@ -99,7 +108,6 @@ class DatasetTransmission(dataset.Dataset):
         tf.summary.image("image", images)
         return images, transmissions
 
-    #TODO: resolver o problema do batch size variavel
     def next_batch_test(self):
         """
         returns:
@@ -110,10 +118,10 @@ class DatasetTransmission(dataset.Dataset):
 
         filenames = self.test_file
 
-        dataset = tf.contrib.data.TFRecordDataset(filenames)
-        dataset = dataset.map(self._parse_function)  # Parse the record into tensors.
-        dataset = dataset.batch(self.config_dict["batch_size"])
-        iterator = dataset.make_initializable_iterator()
+        dset = tf.contrib.data.TFRecordDataset(filenames)
+        dset = dset.map(self._parse_function)  # Parse the record into tensors.
+        dset = dset.batch(self.config_dict["batch_size"])
+        iterator = dset.make_initializable_iterator()
         initializer = iterator.initializer
 
         images, transmissions = iterator.get_next()
