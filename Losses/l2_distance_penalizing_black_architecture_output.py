da3b10d596f7667f5c497bfee1cd6c20a7ffe3e2
Unknown <joelfelipe94@gmail.com>
2018-01-29 10:22:10 -0200

    Revert "Auto stash before merge of "master" and "origin/master""
    This reverts commit efd0465f7bce52e060b6017e49e8982689f714f6.

--- Losses/l2_distance_penalizing_black_architecture_output.py
+++
@@ -1,9 +0,0 @@
-import loss
-import tensorflow as tf
-class L2PenalizingBlackArchitectureOutput(loss.Loss):
-    def __init__(self):
-        parameters_list = []
-        self.config_dict = self.open_config(parameters_list)
-    def evaluate(self, architecture_output, target_output):
-        return tf.sqrt(tf.reduce_mean(tf.squared_difference (architecture_output, target_output)*\
-                                      (2-architecture_output)))
