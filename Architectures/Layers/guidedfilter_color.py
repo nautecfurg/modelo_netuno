9d15bbe6ed8338f13e54da87b54d4ce772b1d7d8
Unknown <joelfelipe94@gmail.com>
2018-01-31 10:50:48 -0200

    Auto stash before merge of "master" and "origin/master"

--- Architectures/Layers/guidedfilter_color.py
+++ Architectures/Layers/guidedfilter_color.py
@@ -16,71 +16,50 @@ def guidedfilter_color(I, p, r, eps):
 
     """
 
+    p_shape = p.get_shape().as_list()
 
     w_conv = tf.constant(1.0, dtype=tf.float32, shape=[2*r+1, 2*r+1, 1, 1])
-    ones = tf.ones(p.get_shape()[1:])  #gambiarra para fazer o guided em um batch variavel
-    ones = tf.expand_dims(ones, 0)
+    w_conv3d = tf.constant(1.0, dtype=tf.float32, shape=[2*r+1, 2*r+1, 3, 1])
+    w_conv9d = tf.constant(1.0, dtype=tf.float32, shape=[2*r+1, 2*r+1, 9, 1])
+
+    ones = tf.ones([1]+p_shape[1:])  #gambiarra para fazer o guided em um batch variavel
     N = tf.nn.conv2d(ones, w_conv, strides=[1, 1, 1, 1], padding='SAME')
-    I_r = tf.expand_dims(I[:, :, :, 0], -1)
-    I_g = tf.expand_dims(I[:, :, :, 1], -1)
-    I_b = tf.expand_dims(I[:, :, :, 2], -1)
-    mean_I_r = tf.nn.conv2d(I_r, w_conv, strides=[1, 1, 1, 1], padding='SAME')/N
-    mean_I_g = tf.nn.conv2d(I_g, w_conv, strides=[1, 1, 1, 1], padding='SAME')/N
-    mean_I_b = tf.nn.conv2d(I_b, w_conv, strides=[1, 1, 1, 1], padding='SAME')/N
 
+    mean_I = tf.nn.depthwise_conv2d(I, w_conv3d, strides=[1, 1, 1, 1], padding='SAME')/N 
     mean_p = tf.nn.conv2d(p, w_conv, strides=[1, 1, 1, 1], padding='SAME')/N
-
-    mean_Ip_r = tf.nn.conv2d(I_r*p, w_conv, strides=[1, 1, 1, 1], padding='SAME')/N
-    mean_Ip_g = tf.nn.conv2d(I_g*p, w_conv, strides=[1, 1, 1, 1], padding='SAME')/N
-    mean_Ip_b = tf.nn.conv2d(I_b*p, w_conv, strides=[1, 1, 1, 1], padding='SAME')/N
-
-    # this is the covariance of (I, p) in each local patch.
-    cov_Ip_r = mean_Ip_r - mean_I_r*mean_p
-    cov_Ip_g = mean_Ip_g - mean_I_g*mean_p
-    cov_Ip_b = mean_Ip_b - mean_I_b*mean_p
+    mean_Ip = tf.nn.depthwise_conv2d(I*p, w_conv3d, strides=[1, 1, 1, 1], padding='SAME')/N
+    cov_Ip = mean_Ip - mean_I*mean_p 
+    cov_Ip = tf.expand_dims(cov_Ip, axis=-1)
 
     # variance of I in each local patch: the matrix Sigma in Eqn (14).
     # Note the variance in each local patch is a 3x3 symmetric matrix:
     #           rr, rg, rb
     #   Sigma = rg, gg, gb
     #           rb, gb, bb
-    mean_I_rr = mean_I_r * mean_I_r
-    mean_I_rg = mean_I_r * mean_I_g
-    mean_I_rb = mean_I_r * mean_I_b
-    mean_I_gg = mean_I_g * mean_I_g
-    mean_I_gb = mean_I_g * mean_I_b
-    mean_I_bb = mean_I_b * mean_I_b
+    
+    mean_I_expand = tf.expand_dims(mean_I, -1)
+    mean_II = tf.matmul(mean_I_expand, mean_I_expand, transpose_a=False, transpose_b=True)
 
-    var_I_rr = tf.nn.conv2d(I_r*I_r, w_conv, strides=[1, 1, 1, 1], padding='SAME')/N - mean_I_rr
-    var_I_rg = tf.nn.conv2d(I_r*I_g, w_conv, strides=[1, 1, 1, 1], padding='SAME')/N - mean_I_rg
-    var_I_rb = tf.nn.conv2d(I_r*I_b, w_conv, strides=[1, 1, 1, 1], padding='SAME')/N - mean_I_rb
-    var_I_gg = tf.nn.conv2d(I_g*I_g, w_conv, strides=[1, 1, 1, 1], padding='SAME')/N - mean_I_gg
-    var_I_gb = tf.nn.conv2d(I_g*I_b, w_conv, strides=[1, 1, 1, 1], padding='SAME')/N - mean_I_gb
-    var_I_bb = tf.nn.conv2d(I_b*I_b, w_conv, strides=[1, 1, 1, 1], padding='SAME')/N - mean_I_bb
+    I_expand = tf.expand_dims(I, -1)
+    II = tf.matmul(I_expand, I_expand, transpose_a=False, transpose_b=True)
+    II_reshaped = tf.reshape(II, [-1] + p.get_shape().as_list()[1:3]+[9])
+    var_I_reshaped = tf.nn.depthwise_conv2d(II_reshaped, w_conv9d, strides=[1, 1, 1, 1], 
+                                            padding='SAME')/N
+    var_I = tf.reshape(var_I_reshaped, p.get_shape().as_list()[1:3]+[3, 3])
+    var_I = var_I - mean_II
 
-    sigma1 = tf.concat([var_I_rr+eps, var_I_rg, var_I_rb], 3)
-    sigma2 = tf.concat([var_I_rg, var_I_gg+eps, var_I_gb], 3)
-    sigma3 = tf.concat([var_I_rb, var_I_gb, var_I_bb+eps], 3)
+    sigma = var_I + (tf.eye(3,batch_shape=p.get_shape()[1:3]) * eps)
 
-    sigma1 = tf.expand_dims(sigma1, -1)
-    sigma2 = tf.expand_dims(sigma2, -1)
-    sigma3 = tf.expand_dims(sigma3, -1)
-    sigma = tf.concat([sigma1, sigma2, sigma3], 4)
     sigma_inv = tf.matrix_inverse(sigma)
 
-    cov_Ip_r = tf.expand_dims(cov_Ip_r, -1)
-    cov_Ip_g = tf.expand_dims(cov_Ip_g, -1)
-    cov_Ip_b = tf.expand_dims(cov_Ip_b, -1)
-    cov_Ip = tf.concat([cov_Ip_r, cov_Ip_g, cov_Ip_b], 4)
+    a = tf.matmul(sigma_inv, cov_Ip, transpose_a=True)
+    
+    a = tf.squeeze(a, 4)        #reduce to rank 4 tensor
 
-    a = tf.matmul(cov_Ip, sigma_inv)
+    b = mean_p - tf.reduce_sum(a*mean_I, axis=3, keep_dims=True)
 
-    b = mean_p - a[:, :, :, :, 0] * mean_I_r - a[:, :, :, :, 1] *\
-        mean_I_g - a[:, :, :, :, 2]*mean_I_b
-
-    q = (I_r* tf.nn.conv2d(a[:, :, :, :, 0], w_conv, strides=[1, 1, 1, 1], padding='SAME') \
-        + I_g * tf.nn.conv2d(a[:, :, :, :, 1], w_conv, strides=[1, 1, 1, 1], padding='SAME') \
-        + I_b * tf.nn.conv2d(a[:, :, :, :, 2], w_conv, strides=[1, 1, 1, 1], padding='SAME') \
+    aI = I*tf.nn.depthwise_conv2d(a, w_conv3d, strides=[1, 1, 1, 1], padding='SAME')
+    
+    q = (tf.reduce_sum(aI,axis=3,keep_dims=True) \
         + tf.nn.conv2d(b, w_conv, strides=[1, 1, 1, 1], padding='SAME'))/N
-
     return q
